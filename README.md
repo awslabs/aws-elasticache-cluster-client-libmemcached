fde977d149ac301f10c3d1f99e56abbe118bc522
danzamansky <danzamansky@users.noreply.github.com>
2015-07-28 19:04:06 -0700

    Update license and some minor clean ups.

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Run the following commands to compile the libmemcached library:
 configure
 make
 make install
+
+# Resources
+---------
+ * [Github link] (https://github.com/awslabs/aws-elasticache-cluster-client-libmemcached)
+ * [AmazonElastiCache Auto Discovery](http://docs.amazonwebservices.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html)
+ * [php-memcached] (https://github.com/php-memcached-dev/php-memcached)
+ * [libmemcached](http://tangent.org/552/libmemcached.html)
+ * [memcached](http://www.danga.com/memcached/)
+ * [igbinary](https://github.com/phadej/igbinary/)
--- libtest/unittest.cc
+++ libtest/unittest.cc
@@ -1,4 +1,4 @@
-/*  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+/*
  *
  *  Data Differential YATL (i.e. libtest)  library
  *
@@ -11,27 +11,16 @@
  *      * Redistributions of source code must retain the above copyright
  *  notice, this list of conditions and the following disclaimer.
  *
- *      * Redistributions in binary form must reproduce the above
- *  copyright notice, this list of conditions and the following disclaimer
- *  in the documentation and/or other materials provided with the
- *  distribution.
+ * Portions Copyright 2012-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
- *      * The names of its contributors may not be used to endorse or
- *  promote products derived from this software without specific prior
- *  written permission.
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is located at
  *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *    http://aws.amazon.com/apache2.0/
  *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 #include <config.h>
